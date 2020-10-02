89d67ca564b7c74d1c45f38ed311cd44d01978ef
umarfakhr4y <umarfakhr4y@gmail.com>
2020-10-02 13:33:29 +0700

    nampilin data

--- src/Page/Home/index.jsx
+++ src/Page/Home/index.jsx
@@ -1,9 +1,54 @@
 import React from "react";
+import Axios from 'axios';
+import {config} from "../../config";
 
 const Home = () => {
+  const [list, setList] = React.useState([]);
+  React.useEffect(() => {
+    getList();
+  }, [])
+
+const getList = async() =>{
+  try{
+
+    let response = await Axios.get(`${config.api_host}/users?page=1`)
+    setList(response.data.data)
+  } catch{
+    console.log("error");
+  }
+}
   return (
     <React.Fragment>
-      <h3>Home</h3>
+      <div className="container">
+        <table className="table table-responsive table-striped">
+          <thead>
+            <tr>
+              <th>Nomor</th>
+              <th>Details</th>
+              <th>Avatar</th>
+              <th>Email</th>
+              <th>First Name</th>
+              <th>Last Name</th>  
+            </tr>
+          </thead>
+          <tbody>
+            {list.map((list, index) =>
+              <tr key={list.id}>
+                <td>{index+1}</td>
+                <td>
+                  <button className="btn btn-primary">Button</button>
+                </td>
+                <td>
+                  <img className="rounded-circle" src={list.avatar} alt=""/>
+                </td>
+                <td>{list.email}</td>
+                <td>{list.first_name}</td>
+                <td>{list.last_name}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
     </React.Fragment>
   );
 };
